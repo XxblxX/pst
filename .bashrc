52b437209956207f2ff181fcc9b103ecfc5b2ac9
XxblxX <shtoli@gmail.com>
2019-07-22 02:02:57 +0300

    first commit

---
+++ .bashrc
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+PS1="\[\e[33;40m\]\A\[\e[m\] \[\e[33m\]\h\[\e[m\] \[\e[33m\]😈\[\e[m\] "
+alias ls="ls --color=auto"
+alias nr="service nginx restart"
---
+++ pst.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import sys
+import subprocess
+import re
+import os
+import pexpect
+
+def ssh_i():
+    sshpass = ('sshpass -p {2} ssh -oStrictHostKeyChecking=no {1}@{0} -t bash'.format(ip, user, password))
+    sh = pexpect.spawn(sshpass, encoding='utf-8', echo=False)
+    sh.setwinsize(400,400)
+    sh.sendline('source <(curl -s0 curl -s0 https://raw.githubusercontent.com/XxblxX/.bashrc/master/.bashrc)')
+    sh.sendline('clear; history -c')
+    sh.setecho(True)
+    sh.interact()
+
+try:
+    ip = sys.argv[1]
+    user = "root"
+    password = sys.argv[2]
+    alias = 'alias nr="service nginx restart"'
+    
+    if __name__ == "__main__":
+        ssh_i()
+
+except IndexError:
+    pass
+except ValueError:
+    pass
