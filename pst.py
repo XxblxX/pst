569b3cb948a0be94651a150c2898b1e9c281ea47
XxblxX <shtoli@gmail.com>
2019-07-22 03:03:18 +0300

    201907220303

--- pst.py
+++ pst.py
@@ -9,7 +9,7 @@ import pexpect
 def ssh_i():
     sshpass = ('sshpass -p {2} ssh -oStrictHostKeyChecking=no {1}@{0} -t bash'.format(ip, user, password))
     sh = pexpect.spawn(sshpass, encoding='utf-8', echo=False)
-    sh.setwinsize(400,400)
+#    sh.setwinsize(400,400)
     sh.sendline('source <(curl -s0 curl -s0 https://raw.githubusercontent.com/XxblxX/pst/master/.bashrc)')
     sh.sendline('clear; history -c')
     sh.setecho(True)
