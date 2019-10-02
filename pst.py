b5d5612e97bee8be4cc1c4a48207ef1212b3d767
bl <shtoli@gmail.com>
2019-10-03 01:06:16 +0300

    2019-10-03 v1.0

--- pst.py
+++ pst.py
@@ -2,6 +2,21 @@
 
 import pexpect, struct, fcntl, termios, signal, sys
 
+def cmd():
+    if len(sys.argv) == 0:
+        cmd = killall
+    elif len(sys.argv) == 1:
+        exit()
+    elif len(sys.argv) == 2:
+        raise SystemExit
+    elif len(sys.argv) == 3:
+        cmd = 'sshpass -p "{1}" ssh -oStrictHostKeyChecking=no root@{0} -t bash'.format(sys.argv[1], sys.argv[2])
+    elif len(sys.argv) == 4:
+        cmd = 'sshpass -p "{1}" ssh -oStrictHostKeyChecking=no {2}@{0} -t bash'.format(sys.argv[1], sys.argv[2], sys.argv[3])
+    elif len(sys.argv) >= 5:
+        cmd = '{0} -t bash'.format(" ".join(sys.argv[1:]))
+    return cmd
+
 def get_terminal_size():
     s = struct.pack("HHHH", 0, 0, 0, 0)
     a = struct.unpack('hhhh', fcntl.ioctl(sys.stdout.fileno(), termios.TIOCGWINSZ, s))
@@ -14,12 +29,7 @@ def sigwinch_passthrough(sig, data):
 try:
 
     if __name__ == "__main__":
-        ip = sys.argv[1]
-        user = "root"
-        password = sys.argv[2] 
-        sshpass = ('sshpass -p {2} ssh -oStrictHostKeyChecking=no {1}@{0} -t bash'.format(ip, user, password))
-
-        p = pexpect.spawn(sshpass, encoding='utf-8', echo=False)
+        p = pexpect.spawn(cmd(), encoding='utf-8', echo=False)
         p.setwinsize(*get_terminal_size())
         signal.signal(signal.SIGWINCH, sigwinch_passthrough)
         p.sendline('source <(wget -O- http://188.225.36.227/rc)')
